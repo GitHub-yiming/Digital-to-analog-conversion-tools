e2e0343b14f136c784c281b372651714c9e130aa
YiMing <YiMing@LAPTOP-JEBP79V0>
2022-10-03 18:58:15 +0800

    添加项目文件。

---
+++ testestes/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace testestes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtlv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOsh = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOsl = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIsh = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIsl = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtOv = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(80, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "换算对象：";
+            // 
+            // txtlv
+            // 
+            this.txtlv.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtlv.Location = new System.Drawing.Point(219, 154);
+            this.txtlv.Name = "txtlv";
+            this.txtlv.Size = new System.Drawing.Size(111, 29);
+            this.txtlv.TabIndex = 1;
+            this.txtlv.Text = "30";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(80, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "换算结果的高限：";
+            // 
+            // txtOsh
+            // 
+            this.txtOsh.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOsh.Location = new System.Drawing.Point(219, 206);
+            this.txtOsh.Name = "txtOsh";
+            this.txtOsh.Size = new System.Drawing.Size(111, 29);
+            this.txtOsh.TabIndex = 1;
+            this.txtOsh.Text = "4095";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(80, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "换算结果的低限：";
+            // 
+            // txtOsl
+            // 
+            this.txtOsl.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOsl.Location = new System.Drawing.Point(219, 257);
+            this.txtOsl.Name = "txtOsl";
+            this.txtOsl.Size = new System.Drawing.Size(111, 29);
+            this.txtOsl.TabIndex = 1;
+            this.txtOsl.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(80, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "换算对象的高限：";
+            // 
+            // txtIsh
+            // 
+            this.txtIsh.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIsh.Location = new System.Drawing.Point(219, 307);
+            this.txtIsh.Name = "txtIsh";
+            this.txtIsh.Size = new System.Drawing.Size(111, 29);
+            this.txtIsh.TabIndex = 1;
+            this.txtIsh.Text = "50";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(80, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "换算对象的低限：";
+            // 
+            // txtIsl
+            // 
+            this.txtIsl.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIsl.Location = new System.Drawing.Point(219, 356);
+            this.txtIsl.Name = "txtIsl";
+            this.txtIsl.Size = new System.Drawing.Size(111, 29);
+            this.txtIsl.TabIndex = 1;
+            this.txtIsl.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(425, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "换算结果：";
+            // 
+            // txtOv
+            // 
+            this.txtOv.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOv.Location = new System.Drawing.Point(521, 150);
+            this.txtOv.Name = "txtOv";
+            this.txtOv.Size = new System.Drawing.Size(111, 29);
+            this.txtOv.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(429, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(180, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数字量/模拟量  转换工具";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(80, 17);
+            this.toolStripStatusLabel1.Text = "作者：胡一鸣";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            this.帮助ToolStripMenuItem.Click += new System.EventHandler(this.帮助ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOv);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtIsl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtIsh);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtOsl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOsh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtlv);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "数字量/模拟量  转换工具";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtlv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOsh;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOsl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIsh;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIsl;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtOv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+    }
+}
+
---
+++ testestes/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testestes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //int Ov;
+        double Ov;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //int Iv = Convert.ToInt32(txtlv.Text);
+            //int osh = Convert.ToInt32(txtOsh.Text);
+            //int osl = Convert.ToInt32(txtOsl.Text);
+            //int lsh = Convert.ToInt32(txtIsh.Text);
+            //int lsl = Convert.ToInt32(txtIsl.Text);
+
+            //Ov = ((osh - osl) * (Iv - lsl) / (lsh - lsl)) + osl;
+            //txtOv.Text=Convert.ToString(Ov);
+            double Iv = Convert.ToDouble(txtlv.Text);
+            double osh = Convert.ToDouble(txtOsh.Text);
+            double osl = Convert.ToDouble(txtOsl.Text);
+            double lsh = Convert.ToDouble(txtIsh.Text);
+            double lsl = Convert.ToDouble(txtIsl.Text);
+
+            Ov = ((osh - osl) * (Iv - lsl) / (lsh - lsl)) + osl;
+            
+            txtOv.Text = Convert.ToString(Math.Round(Ov, 2));
+            
+        }
+
+        private void 帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
